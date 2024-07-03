441866f57fead5b7963a7d5eb1f7645854209dde
Borodin Denis <borodin.denis.23@gmail.com>
2024-07-04 00:54:38 +0300

    Добавлено Занятие 2 - Задание 3

---
+++ lesson_2_3.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wb = webdriver.Chrome()
+    request.addfinalizer(wb.quit)
+    return wb
+
+
+def test_autorization(driver):
+    driver.get('http://localhost/litecart/admin/')
+    driver.find_element(By.NAME, 'username').send_keys('admin')
+    driver.find_element(By.NAME, 'password').send_keys('admin')
+    driver.find_element(By.NAME, 'login').click()
