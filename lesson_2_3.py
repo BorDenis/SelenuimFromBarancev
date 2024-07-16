f81ca889451b3733f35f4d0b4c063ed6ac09bd89
Borodin Denis <borodin.denis.23@gmail.com>
2024-07-17 02:07:47 +0300

    Добавлены дополнительные браузеры

--- lesson_2_3.py
+++ lesson_2_3.py
@@ -3,9 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
+
 @pytest.fixture
 def driver(request):
     wb = webdriver.Chrome()
+
+    # options = webdriver.FirefoxOptions()
+    # options.binary_location = 'C:/Program Files/Firefox Nightly/firefox.exe'
+    # wb = webdriver.Firefox(options=options)
+
+    # wb = webdriver.Ie()
+    
     request.addfinalizer(wb.quit)
     return wb
 
