d625dc93cba2e2efb0b518eb21a7368bbe66a38a
Borodin Denis <borodin.denis.23@gmail.com>
2024-07-17 02:36:40 +0300

    Добавлено Занятие 4 - Задание 2

---
+++ lesson_4_2.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture
+def driver(request):
+    wb = webdriver.Chrome()
+
+    # options = webdriver.FirefoxOptions()
+    # options.binary_location = 'C:/Program Files/Firefox Nightly/firefox.exe'
+    # wb = webdriver.Firefox(options=options)
+
+    # wb = webdriver.Ie()
+
+    request.addfinalizer(wb.quit)
+    return wb
+
+
+def test_check_stickers(driver):
+    driver.get('http://localhost/litecart/en/')
+    all_products = driver.find_elements(By.XPATH, "//*[@class='product column shadow hover-light']")
+    for product in all_products:
+        stickers = product.find_elements(By.XPATH, ".//div[contains(@class,'sticker')]")
+        assert len(stickers) == 1
