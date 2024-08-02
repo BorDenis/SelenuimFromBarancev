eeb1593bf35124f4606d19c8382b2ee329e3913c
Borodin Denis <borodin.denis.23@gmail.com>
2024-08-03 00:03:46 +0300

    Подчистил код

--- lesson_7_1.py
+++ lesson_7_1.py
@@ -8,14 +8,14 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 @pytest.fixture
 def driver(request):
-    # wb = webdriver.Chrome()
+    wb = webdriver.Chrome()
 
     # options = webdriver.FirefoxOptions()
     # options.binary_location = 'C:/Program Files/Firefox Nightly/firefox.exe'
     # wb = webdriver.Firefox(options=options)
     # wb = webdriver.Firefox()
 
-    wb = webdriver.Ie()
+    # wb = webdriver.Ie()
 
     request.addfinalizer(wb.quit)
     return wb
@@ -37,7 +37,6 @@ def test_add_new_product(driver):
         add_to_cart = driver.find_element(By.XPATH, '//button[@name="add_cart_product"]')
         add_to_cart.click()
         wait.until(EC.text_to_be_present_in_element((By.XPATH, '//span[@class="quantity"]'), str(i)))
-        # assert driver.find_element(By.XPATH, f'//span[@class="quantity"][text()={i}]')
         driver.back()
     cart_checkout = driver.find_element(By.XPATH, '//div[@id="cart"]/a[@class="link"]')
     cart_checkout.click()
